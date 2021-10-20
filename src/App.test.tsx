2c5b5955185efa3fd92892bae5673c826ec91469
Trisno Nino <trisno@mola.tv>
2021-10-20 20:48:41 +0700

    Fixing unit testing app.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,12 +4,12 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders movie stock link', () => {
   const { getByText } = render(
     <Provider store={store}>
       <App />
     </Provider>
   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(getByText(/MovieStock/i)).toBeInTheDocument();
 });
